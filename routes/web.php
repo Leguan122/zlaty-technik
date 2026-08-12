fd8e590b3866c84294b4fac899525a92f54f38f7
Leguan122 <104968100+Leguan122@users.noreply.github.com>
2026-08-12 18:33:19 +0200

    Add multipage routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home')->name('home');
+Route::view('/opravy', 'repairs')->name('repairs');
+Route::view('/weby-it', 'web-it')->name('web-it');
+Route::view('/iot', 'iot')->name('iot');
+Route::view('/kontakt', 'contact')->name('contact');
