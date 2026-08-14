04dac59d0dab363be9d0eb7498978aff75a83f4a
Leguan122 <104968100+Leguan122@users.noreply.github.com>
2026-08-14 17:32:37 +0200

    Add 3D service route and focus repair service on computers

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
-Route::view('/opravy', 'repairs')->name('repairs');
 Route::view('/weby-it', 'web-it')->name('web-it');
 Route::view('/iot', 'iot')->name('iot');
+Route::view('/3d', '3d')->name('3d');
+Route::view('/pocitace', 'repairs')->name('repairs');
 Route::view('/kontakt', 'contact')->name('contact');
